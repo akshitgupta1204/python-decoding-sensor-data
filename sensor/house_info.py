19f27a0ab671435878d56f3041faab8434637050
akshitgupta1204 <32005721+akshitgupta1204@users.noreply.github.com>
2022-08-13 09:57:12 -0700

    Module 2

---
+++ sensor/house_info.py
@@ -0,0 +1,24 @@
+from datetime import date, datetime
+class HouseInfo:
+    def __init__(self, data):
+        self.data = data
+
+    def get_data_by_area(self, field, rec_area=0):
+        # print("field is: " + field)
+        # print("rec_area is: " + str(rec_area))
+        field_data = []
+        for record in self.data:
+            # print(type(self.data))
+            
+            if rec_area == 0:
+                field_data.append(record[field])
+            elif rec_area == int(record['area']):
+                field_data.append(record[field])
+        return field_data              
+
+    def get_data_by_date(self, field, rec_date=date.today()):
+        field_date = []
+        for record in self.data:
+            if record['date'] == date.strftime(rec_date, "%m/%d/%y"):
+                field_date.append(record['date'])
+        return field_date        
